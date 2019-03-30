6be40171deb063f8dd8d8fcfb30c1d9fcc946712
hung <hung150896@gmail.com>
2019-03-30 10:48:16 +0700

    config server

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,23 +1,27 @@
 /* eslint-disable indent */
 /* eslint-disable no-undef */
 const devConfig = {
+	HOST: process.env.HOST || 'localhost',
+	PORT: process.env.PORT || 3000,
 	MONGO_URL:
 		'mongodb://hungdm:30c15b7800213b2844e224a1f5fbdfb0@43.239.223.206:27017/gr2019'
 }
 
 const testConfig = {
+	HOST: process.env.HOST || 'localhost',
+	PORT: process.env.PORT || 3000,
 	MONGO_URL:
 		'mongodb://hungdm:30c15b7800213b2844e224a1f5fbdfb0@43.239.223.206:27017/gr2019'
 }
 
 const prodConfig = {
+	HOST: process.env.HOST || '43.239.223.206',
+	PORT: process.env.PORT || 3000,
 	MONGO_URL:
 		'mongodb://hungdm:30c15b7800213b2844e224a1f5fbdfb0@43.239.223.206:27017/gr2019'
 }
 
 const defaultConfig = {
-	HOST: process.env.HOST || 'localhost',
-	PORT: process.env.PORT || 3000,
 	API_PREFIX: '/api',
 	JWT_SECRET: 'hihihihihihihi',
 	FACEBOOK_APP_ID: '329324544364004',
