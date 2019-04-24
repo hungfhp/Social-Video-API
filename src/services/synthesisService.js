0feb90e739cba6bbc99cd1f7e71103c0fa15af46
hung <hung150896@gmail.com>
2019-04-24 18:30:44 +0700

    done callback voiceover

--- src/services/synthesisService.js
+++ src/services/synthesisService.js
@@ -1,9 +1,35 @@
+/* eslint-disable no-console */
 import axios from 'axios'
 // import HTTPStatus from 'http-status'
 
-export function requestSynthesis(movieId, subUrl) {}
+export async function requestSynthesis(
+	subUrl,
+	voice = 'hn_male_xuantin_vdts_48k-hsmm'
+) {
+	let url = 'http://api.thuyetminhphim.vn/synthesis'
+	let callback = 'http://api-social.thuyetminhphim.com/api/voiceovers/callback'
+	return await axios
+		.get(`${url}?callback=${callback}&subtitle_url=${subUrl}&voice=${voice}`)
+		.then(function(response) {
+			return response.data
+		})
+		.catch(function(e) {
+			console.log('error', e)
+			throw e
+		})
+}
 
-export function requestResynthesis(movieId, subUrl) {}
+export async function requestResynthesis(requestId) {
+	return await axios
+		.get(`http://api.thuyetminhphim.vn/resynthesis?request_id=${requestId}`)
+		.then(function(response) {
+			return response.data
+		})
+		.catch(function(e) {
+			console.log('error', e)
+			throw e
+		})
+}
 
 export async function checkSynthesis(requestId) {
 	return await axios
